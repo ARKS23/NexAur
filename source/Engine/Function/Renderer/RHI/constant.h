be464959e3e06af384ce77867b736d3865733a7a
ARKS23 <74123700+ARKS23@users.noreply.github.com>
2026-01-23 21:06:32 +0800

    删除渲染模块相关文件，重新设计

--- source/Engine/Function/Renderer/RHI/constant.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace NexAur::RHI{
-    constexpr int MAX_RENDER_TARGETS = 8;
-}; // namespace NexAur::RHI
--- source/Engine/Function/Renderer/RHI/render_pass.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// TODO: render pass之后再进行设计
-
-namespace NexAur {
-    class IRenderPass {
-
-    };
-} // namespace NexAur
